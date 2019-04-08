2477e0e15ab0d98ecdf6c79bc5351911a8371333
prapawit201 <piyapat43@gmail.com>
2019-04-08 18:53:01 +0700

    [EDIT]car , customer table

--- ZProJInt207(New).sql
+++ ZProJInt207(New).sql
@@ -4,21 +4,22 @@ select * from user_sys_privs;
 
 revoke create session from project;
 
-drop table Staff;
-drop table car;
-drop table branch;
-drop table customer;
-drop table RENT;
-drop table ZPROJMaintenance; 
+drop table ZRSPROJBRANCH;
+drop table ZRSPROJcar;
+drop table ZRSPROJstaff;
+drop table ZRSPROJcustomer;
+drop table ZRSPROJRENT;
+drop table ZRSPROJMaintenance; 
 
-create table ZProJBranch (
+create table ZRSPROJBranch (
 branchNo varchar2(13) not null,
+branchName varchar2(20) not null,
 province varchar2(40) not null,
 address varchar2(40) not null,
 constraint pk_Branch_branchNo primary key (branchNo)
 );
 
-create table ZProJCustomer (
+create table ZRSPROJCustomer (
 customerNo varchar2(13) not null,
 fname varchar2(40) not null,
 lname varchar2(40) not null,
@@ -32,7 +33,7 @@ password varchar2(40) not null,
 constraint pk_Customer_customerNo primary key (customerNo)
 );
 
-create table ZProJMaintenance (
+create table ZRSPROJMaintenance (
 maintNo varchar2(13) not null,
 maintDate date not null,
 maintPrice number(5) not null,
@@ -42,7 +43,7 @@ maintDetail varchar2(100) not null,
 constraint pk_Maintenance_maintNo primary key (maintNo)
 );
 
-create table ZProJStaff (
+create table ZRSPROJStaff (
 StaffNo number(13) not null,
 Fname varchar2(40) not null,
 Lname varchar2(40) not null,
@@ -50,30 +51,30 @@ Position varchar2(20) not null,
 Salary decimal(10,2) not null,
 branchNo varchar2(13) not null,
 constraint pk_Staff_StaffNo primary key (StaffNo),
-constraint fk_Branch_branchNo foreign key (branchNo) references ZProJBranch(branchNo)
+constraint fk_Branch_branchNo foreign key (branchNo) references ZRSPROJBranch(branchNo)
 );
 
 
-create table ZPROJCar (
+create table ZRSPROJCar (
 CarNo varchar2(13) not null,
 Brand varchar2(10) not null,
-CarSize varchar2(3) not null check (CarSize = 'S' OR CarSize = 'M' OR CarSize = 'L' OR CarSize = 'XL' ),
+CarSize varchar2(3) not null check (CarSize = 'S' OR CarSize = 'M' OR CarSize = 'L' ),
 Lugguage number(1) not null check (Lugguage = 1 OR Lugguage = 2 OR Lugguage = 3 OR Lugguage = 4 ),
-Salary decimal(10,2) not null,
-Seat number(2)not null check (Seat = 2 OR Seat = 5 OR Seat = 7 OR Seat = 15 ),
-CarType varchar2(40)not null check (CarType = 'Saloon' OR CarType = 'Pickup' OR CarType = 'Van' OR CarType = 'SUV' ),
-Fuel varchar2(10)not null,
+Price decimal(10,2) not null,
+Seat number(2)not null check (Seat = 2 OR Seat = 5 OR Seat = 7 ),
+CarType varchar2(40)not null check (CarType = 'Saloon' OR CarType = 'Pickup'  OR CarType = 'SUV' ),
+--Fuel varchar2(10)not null,
 --Engine decimal(1,2)not null,
 Engine varchar2(5)not null check (Engine = 'Diesel' OR Engine = 'Benzine'),
 Transmission varchar2(10)not null check (Transmission = 'Manual' OR Transmission = 'Auto'),
 branchNo varchar2(13) not null,
 maintNo varchar2(13) not null,
 constraint pk_Car_CarNo primary key (CarNo),
-constraint fk_Car_Branchno foreign key (branchNo) references ZProJBranch(Branchno),
-constraint fk_Car_MaintNo foreign key (maintNo) references ZProJMaintenance(maintNo)
+constraint fk_Car_Branchno foreign key (branchNo) references ZRSProJBranch(Branchno),
+constraint fk_Car_MaintNo foreign key (maintNo) references ZRSProJMaintenance(maintNo)
 );
 
-create table ZProJRent (
+create table ZRSPROJRent (
 rentNo varchar2(13) not null,
 pickDate date not null,
 returnDate date not null,
@@ -82,8 +83,12 @@ returnBranch varchar2(40) not null,
 price number(5) not null,
 delayPenalty varchar2(40),
 customerNo varchar2(13) not null,
+CarNo varchar2(13) not null,
+StaffNo number(13) not null,
 constraint pk_Rent_rentNo primary key (rentNo),
-constraint fk_Rent_customerNo foreign key (customerNo) references ZProJCustomer(customerNo)
+constraint fk_Rent_customerNo foreign key (customerNo) references ZRSProJCustomer(customerNo),
+constraint fk_Rent_CarNo foreign key (CarNo) references ZRSPROJCar(CarNo),
+constraint fk_Rent_StaffNo foreign key (StaffNo) references ZRSPROJStaff(StaffNo)
 );
 
 
